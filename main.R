fc15f83e86c701298ef749da7ed634e37180c360
Kris Tokarz <kj.tokarz@pm.me>
2022-11-09 08:51:14 -0500

    Add comments to outline what we need to do.

--- main.R
+++ main.R
@@ -1,1 +1,12 @@
-data = read.csv("./data.csv")
+# Read in Data. Using this nba data as a placeholder.
+dat = read.csv("./nba_logreg.csv")
+
+# 1. Draw graphs to interpret the property of the data. What do you find?
+
+# 2. Select and fit the model. Summarize model results
+
+# 3. Analyze the contribution of each predictor
+
+# 4. Check the model adequacy
+
+# 5. Summarize the analysis results
