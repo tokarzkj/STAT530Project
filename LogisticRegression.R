5b76ea1462ec80579fc1ac22d20070fe9a8469f1
Kris Tokarz <kj.tokarz@pm.me>
2022-11-10 06:29:36 -0500

    Replace my initial data with something more straight-forward.
    The Fish Market Data is pretty self explanatory. I got it off a site
    called Kaggle. I also downloaded a set of data called Candy, which
    can be analyzed using Logistic Regression.
    
    I started the fish market sample and it is pretty standard to our normal
    labs.

---
+++ LogisticRegression.R
@@ -0,0 +1,5 @@
+# Because I am extra here is a logistic regression file that we can play with.
+
+candyData = read.csv("./candy-data.csv")[2:13]
+attach(candyData)
+pairs(candyData)
--- main.R
+++ main.R
@@ -1,9 +1,19 @@
-# Read in Data. Using this nba data as a placeholder.
-dat = read.csv("./nba_logreg.csv")
+# This file contains a very straight-forward example using the Fish Market. 
+# Probably the best option if we want to get the project done quick.
 
+
+# Read in Data. Using this fish market as a placeholder.
+fishData = read.csv("./Fish.csv")[2:7]
+attach(fishData)
 # 1. Draw graphs to interpret the property of the data. What do you find?
 
+# Species name breaks pairs, so I ignored the first column when setting up our data.
+pairs(fishData)
+
 # 2. Select and fit the model. Summarize model results
+
+model <- lm(formula = Weight ~ ., data = fishData)
+summary(model)
 
 # 3. Analyze the contribution of each predictor
 
