05aabaace90ccd9485e4685f27f10096b156dc4b
Kris Tokarz <kj.tokarz@pm.me>
2022-11-10 11:38:12 -0500

    Include doc links

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -2,4 +2,7 @@
 
 candyData = read.csv("./candy-data.csv")[2:13]
 attach(candyData)
-pairs(candyData)
+pairs(formula = ~ sugarpercent + pricepercent + winpercent, data = candyData)
+
+model <- lm(formula = winpercent ~ ., data = candyData)
+summary(model)
--- main.R
+++ main.R
@@ -1,6 +1,9 @@
 # This file contains a very straight-forward example using the Fish Market. 
 # Probably the best option if we want to get the project done quick.
-
+# Link to Google Docs I started:
+# https://docs.google.com/document/d/1f98D2EjpBOp1ExCzr1Fxuy8Vmk6Tbwh0-8Ihsb91nc0/edit?usp=sharing
+# If we want to be fancy we can use this latex writer
+# https://www.overleaf.com/6297936779xqkywtccqbrc
 
 # Read in Data. Using this fish market as a placeholder.
 fishData = read.csv("./Fish.csv")[2:7]
@@ -16,6 +19,16 @@ model <- lm(formula = Weight ~ ., data = fishData)
 summary(model)
 
 # 3. Analyze the contribution of each predictor
+
+# Should we use t-test and f-test here?
+
+# Significance of Regression
+model2 <- lm(formula = Weight ~ 1., data = fishData)
+anova(model2, model)
+
+alpha = 0.05
+qf(1 - alpha, 5, 153)
+
 
 # 4. Check the model adequacy
 
