525702fc254565f6a243a39153ce9d4ae0c30ad0
Kris Tokarz <kj.tokarz@pm.me>
2022-11-12 16:19:55 -0500

    Add t obs and an explanation

--- main.R
+++ main.R
@@ -7,7 +7,6 @@
 
 # Read in Data. Using this fish market as a placeholder.
 fishData = read.csv("./Fish.csv")[2:7]
-attach(fishData)
 # 1. Draw graphs to interpret the property of the data. What do you find?
 
 # Species name breaks pairs, so I ignored the first column when setting up our data.
@@ -20,15 +19,46 @@ summary(model)
 
 # 3. Analyze the contribution of each predictor
 
-# Should we use t-test and f-test here?
+# Use tobs to test individual contributions
+
+# b1 = 0 - Length1
+b1_tobs = (62.355 - 0)/40.209
+
+# b2 = 0 - Length2
+b2_tobs = (-6.527 - 0)/41.759
+
+# b3 = 0 - Length3
+b3_tobs = (-29.026 - 0)/17.353
+
+# b4 = 0 - Height
+b4_tobs = (28.297 - 0)/8.729
+
+# b5 = 0 - Width
+b5_tobs = (22.473 - 0)/20.372
+
+print(b1_tobs)
+print(b2_tobs)
+print(b3_tobs)
+print(b4_tobs)
+print(b5_tobs)
+
+n = nrow(fishData)
+k = 6
+qt(1 - 0.05/2, n - k)
+
+# Reviewing page 88 in the book regarding T tests and MLR models it appears that 
+# Height and Width contribute the most to his model relative to the other regressors.
+
+model.test <- lm(formula = Weight ~ Height + Width, data = fishData)
+summary(model.test)
 
 # Significance of Regression
+# This should indicate that at least one of our parameters is linearly significantly related to y.
 model2 <- lm(formula = Weight ~ 1., data = fishData)
 anova(model2, model)
 
 alpha = 0.05
 qf(1 - alpha, 5, 153)
-
 
 # 4. Check the model adequacy
 
